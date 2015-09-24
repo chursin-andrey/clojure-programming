db82c20bc929b32bbce4f2e205fca1584d869be4
Andrey Chursin <andrew.fkn@yandex.ru>
2015-09-24 17:39:50 +0400

    Added additional features such as diff, filter with anonymous functions. Added predicate divided-by-two-without-rest?

--- src/FunSet.clj
+++ src/FunSet.clj
@@ -1,6 +1,9 @@
 ;returns set of an one element
 (defn singleton-set [x] (fn [elem] (= x elem)))
 
+;Displays the contents of a set
+(defn to-string [s] (reduce #(str %1 "," %2) s))
+
 ;checks if the set contains an element
 (defn contains? [x set] (some (partial = x) set))
 
@@ -41,8 +44,15 @@
 
 (diff #{1,2,3,4,5} [4,5,6])
 
-;Displays the contents of a set
-(defn to-string [s] (reduce #(str %1 "," %2) s))
+(defn diff [s t]
+  (if (< (count s) (count t))
+    (reduce #(if (contains? t %2)
+                (disj %1 %2)
+                %1)
+      s t)
+    (reduce disj s t)))
+
+(diff #{1,2,3,4,5} [4,5,6])
 
 ;Returns the subset of `s` for which `p` holds.
 (defn filter [s f] ()) ;implement
@@ -54,16 +64,19 @@
 
 (filter-set even? #{1 2 3 4 5 6})
 
-;The bounds for `forall` and `exists` are +/- 1000.
-(def bound 1000)
+(defn filter-set
+  [pred set]
+  (reduce #(if (pred %2) %1 (disj %1 %2))
+    set set))
 
-;Returns whether all bounded integers within `s` satisfy `p`.
-(defn forall [s f]()) ;implement
+(filter-set even? #{1 2 3 4 5 6})
 
-;Returns whether there exists a bounded integer within `s`
-;that satisfies `p`.
-(defn exists [s f] ()) ;implement
+(defn divided-by-two-without-rest? [n]
+  (if (integer? n)
+    (if (= (mod n 2) 0) true false)
+    (throw (IllegalArgumentException. (str "Argument must be an integer: " n))))
+  )
 
-;Returns a set transformed by applying `f` to each element of `s`.
-(defn map [s f] ()) ;implement
+(divided-by-two-without-rest? 101)
 
+(filter-set divided-by-two-without-rest? (set (range 1 100)))
